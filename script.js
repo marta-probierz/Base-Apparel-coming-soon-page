98ceb35dc3bf5ab98dd39326e41ab8e5344de5ec
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2021-07-28 19:20:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let form = document.querySelector('form')
+let email = document.querySelector('input[type=email]');
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    let emailValue = email.value;
+    //console.log(emailValue);
+
+    if (validateEmail(emailValue)) {
+        form.classList.remove('error');
+    } else {
+        form.classList.add('error');
+    }
+});
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+
+
+
